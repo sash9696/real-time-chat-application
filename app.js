93115fb0991b194812c659e1ac02776e7a3bbc6e
Sahil Chopra <sahilchopra838@gmail.com>
2025-01-01 17:35:19 +0530

    added mongo db connection

--- app.js
+++ app.js
@@ -2,37 +2,44 @@ import express from 'express';
 import http from 'http';
 import { Server as socketio } from 'socket.io'; 
 import cors from 'cors';
-
+import mongoose from 'mongoose';
 const SERVER_NAME = process.env.SERVER_NAME || 'APP';
 
 const app = express();
 
+
+const uri= 'mongodb+srv://sahilchopra9696:Hwjtp6VpV2LxknKX@cluster0.r2ql7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 const httpServer = http.createServer(app);
+mongoose.connect(uri)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
 const io = new socketio(httpServer, {
   cors: {
-    origin: "*",  
+    origin: "*",  // Allow all origins (for testing)
     methods: ["GET", "POST"]
   }
 });
-
 
 app.use(cors());
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+  
+  socket.on('message', (data) => {
+    console.log('Received message: ', data);
+    socket.emit('message', { response: 'Message received!' });
+  });
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 });
 
-// Example API endpoint
 app.get('/', (req, res) => {
   res.send('Welcome to the Chat App!');
 });
 
-// Start the server
 const PORT = process.env.PORT || 8080;
 httpServer.listen(PORT, () => {
   console.log(`${SERVER_NAME} is running on port ${PORT}`);
